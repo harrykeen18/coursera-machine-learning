1e6706ef28de94616f02f17434619d9ac57bdcda
Harry Keen <harrykeen18@gmail.com>
2017-01-03 21:40:48 +0000

    sigmoid

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -12,13 +12,21 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+% for i = 1:length(X(:,1))
+%     if y(i,1) == 1
+%         plot(X(i,1), X(i,2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+%     else
+%         plot(X(i,1), X(i,2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7)
+%     end
+% end
 
-
-
-
-
-
-
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
+     'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
+   'MarkerSize', 7);
 
 % =========================================================================
 
--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -2,16 +2,21 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
-g = zeros(size(z));
+% You need to return the following variables correctly
+A = size(z);
+g = zeros(A);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
+for col = 1 : A(1,2)
 
+    for row = 1 : A(1,1)
 
+        g(row, col) = 1 / (1 + exp(-z(row, col)));
 
-
+    end
+end
 
 % =============================================================
 
